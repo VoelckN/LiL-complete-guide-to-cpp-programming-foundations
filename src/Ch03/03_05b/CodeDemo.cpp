f2a962b0b77a60519090980f5a5da6568145e074
VoelckN <nick.voelckers@gmail.com>
2025-07-19 16:38:07 -0400

    Completed Chapter 3 Exercises

--- src/Ch03/03_05b/CodeDemo.cpp
+++ src/Ch03/03_05b/CodeDemo.cpp
@@ -11,16 +11,26 @@ int main(){
     std::cout << "Initial Ammo: " << ammo << std::endl;
     std::cout << "Initial Zombies Incoming: " << incoming_zombies << std::endl;
 
-    // Addition assignment
-
+    // Addition assignment (variable to be updated on the left)
+    // Pick up new ammo
+    ammo += 10;
+    std::cout << "New ammo: " << ammo << std::endl;
     // Subtraction assignment
-
+    // After weapon use
+    ammo -= 2;
+    std::cout << "ammo after use: " << ammo << std::endl;
     // Multiplication assignment
-
+    // There's more of them than we thought ...
+    incoming_zombies *= 2;
+    std::cout << "Z count after timeout: " << incoming_zombies << std::endl;
     // Division assignment
-
+    // Sharing ammo
+    ammo /= 2;
+    std::cout << "Ammo after sharing with friend: " << ammo << std::endl;
     // Remainder assignment
-
+    // Backpack lost
+    ammo %= 5;
+    std::cout << "Backpack lost - ammo left: " << ammo << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -5,14 +5,26 @@
 #include <iostream>
 #include <cstdint>
 
+// HAHA fun fact: #define is discouraged because of lack of type enforcement
+// instead: use const qualifier
+#define MAX_AMMO 500
+//#define DEBUG //Doesn't need a value if it's only used for an #ifdef directive
+
 int main(){
-    int32_t ammo = 100;
+    // I can't tell if using MAX_AMMO here is just to show
+    int32_t ammo = MAX_AMMO / 5;
     uint8_t health_items = 5;
+
+// useful for debugging
+#ifdef DEBUG
+    std::cout << "[DEBUG] Starting game simulation ..." << std::endl;
+#endif
 
     ammo += 200; // Player finds extra ammo
     health_items -= 2; // Player uses some health items
 
     std::cout << "Final Ammo: " << ammo << std::endl;
+    // int must be cast to health_items (otherwise it would be read as a text char)
     std::cout << "Remaining Health Items: " << (int) health_items << std::endl;
 
     std::cout << std::endl << std::endl;
